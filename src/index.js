3bfc96d6fce68c2764c877fc068f4ff0e727b82b
marmooo <marmooo@users.noreply.github.com>
2023-11-26 21:42:49 +0900

    update

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const dirNames = [
+  "ひらがな",
+  "カタカナ",
+  "小1",
+  "小2",
+  "小3",
+  "小4",
+  "小5",
+  "小6",
+  "中2",
+  "中3",
+  "高校",
+  "常用",
+  "準1級",
+  "1級",
+];
+
+function loadConfig() {
+  if (localStorage.getItem("darkMode") == 1) {
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+  }
+}
+
+function toggleDarkMode() {
+  if (localStorage.getItem("darkMode") == 1) {
+    localStorage.setItem("darkMode", 0);
+    document.documentElement.setAttribute("data-bs-theme", "light");
+  } else {
+    localStorage.setItem("darkMode", 1);
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+  }
+}
+
+function changeGrade(event) {
+  const dir = dirNames[event.target.selectedIndex];
+  location.href = `/graded-vocab-ja/${dir}/`;
+}
+
+loadConfig();
+document.getElementById("toggleDarkMode").onclick = toggleDarkMode;
+document.getElementById("gradeOption").onchange = changeGrade;
